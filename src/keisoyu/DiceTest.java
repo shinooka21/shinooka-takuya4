837dff3e842320bba43eb7cd7c105cb698dcdc8d
renta <renta@192.168.1.148>
2024-03-26 11:42:07 +0900

    修正

--- src/keisoyu/DiceTest.java
+++ src/keisoyu/DiceTest.java
@@ -1,11 +1,12 @@
 package keisoyu;
 
 public class DiceTest {
-	
-	public static void main(String[] args) {
+    
+    public static void main(String[] args) {
         Dice dice = new Dice();
 
-        // 30回実施
+        System.out.println("Dice.javaのテスト");
+     // 30回実施
         for (int i = 0; i < 30; i++) {
             int result = dice.cast();
             int lastResult = dice.see();
@@ -13,6 +14,15 @@ public class DiceTest {
             System.out.println("投げた結果: " + result + "  最後に投げた目: " + lastResult);
         }
         
-	}
+        System.out.println("CheatDiceのテスト");
+        Dice cheatDice = new CheatDice();
 
+        // 30回実施
+        for (int i = 0; i < 30; i++) {
+            int result = cheatDice.cast();
+            int lastResult = cheatDice.see();
+
+            System.out.println("投げた結果: " + result + "  最後に投げた目: " + lastResult);
+        }
+    }
 }
